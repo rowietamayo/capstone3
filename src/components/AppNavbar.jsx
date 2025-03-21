611fcaeb474f1da46dcf9201d9fba23b90292b97
rowimaytamayo <rowimaytamayo@gmail.com>
2025-03-21 14:17:53 +0800

    modify navbar

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -9,61 +9,98 @@ export default function AppNavbar() {
   const { user } = useContext(UserContext)
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="glassmorphism">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <img
-            alt=""
-            src={"/image/logo.png"}
+          {/* <Image
+            alt="logo"
+            src={"/image/logo.svg"}
             width="30"
             height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Capstone 3
+            className="d-inline-block align-top logo"
+            fluid
+          />{" "} */}
+          <h3>OHARA</h3>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/" exact="true">
+          <Nav className="ms-auto ">
+            <Nav.Link as={NavLink} to="/" exact="true" className="right-nav">
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/product" exact="true">
+            {/* <Nav.Link as={NavLink} to="/product" exact="true">
               Products
-            </Nav.Link>
-            {user && user.id !== null && user.id !== undefined ? (
-              user.isAdmin ? (
-                <>
-                  <Nav.Link as={Link} to="/admin">
+            </Nav.Link>{" "} */}
+          </Nav>
+          {user && user.id !== null && user.id !== undefined ? (
+            user.isAdmin ? (
+              <>
+                <Nav>
+                  <Nav.Link as={Link} to="/admin" className="right-nav">
                     Admin Dashboard
                   </Nav.Link>
-                  <Nav.Link as={NavLink} to="/logout" exact="true">
+                  <Nav.Link
+                    as={NavLink}
+                    to="/logout"
+                    exact="true"
+                    className="right-nav"
+                  >
                     Logout
                   </Nav.Link>
-                </>
-              ) : (
-                <>
-                  <Nav.Link as={NavLink} to="/cart" exact="true">
-                    Cart
-                  </Nav.Link>
-                  <Nav.Link as={NavLink} to="/profile" exact="true">
-                    Profile
-                  </Nav.Link>
-                  <Nav.Link as={NavLink} to="/logout" exact="true">
-                    Logout
-                  </Nav.Link>
-                </>
-              )
+                </Nav>
+              </>
             ) : (
               <>
-                <Nav.Link as={NavLink} to="/login" exact="true">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={NavLink} to="/register" exact="true">
+                <Nav>
+                  <Nav.Link
+                    as={NavLink}
+                    to="/cart"
+                    exact="true"
+                    className="right-nav"
+                  >
+                    Cart
+                  </Nav.Link>
+                  <Nav.Link
+                    as={NavLink}
+                    to="/profile"
+                    exact="true"
+                    className="right-nav"
+                  >
+                    Profile
+                  </Nav.Link>
+                  <Nav.Link
+                    as={NavLink}
+                    to="/logout"
+                    exact="true"
+                    className="right-nav"
+                  >
+                    Logout
+                  </Nav.Link>
+                </Nav>
+              </>
+            )
+          ) : (
+            <>
+              <Nav>
+                <Nav.Link
+                  as={NavLink}
+                  to="/register"
+                  exact="true"
+                  className="right-nav"
+                >
                   Register
                 </Nav.Link>
-              </>
-            )}
-          </Nav>
+                <Nav.Link
+                  as={NavLink}
+                  to="/login"
+                  exact="true"
+                  className="right-nav"
+                >
+                  Login
+                </Nav.Link>
+              </Nav>
+            </>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
